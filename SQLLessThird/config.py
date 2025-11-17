e430fb972371571deba768a1d807af1aa45baae5
leokao960811 <leokao960811@gmail.com>
2025-11-17 10:47:11 +0800

    Config refresh

--- SQLLessThird/config.py
+++ SQLLessThird/config.py
@@ -6,4 +6,4 @@ load_dotenv() # Load environment variables from .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ITEM_HERE'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/notes'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://leokao960811:HroIh9Rs9URiF58r@notecluster.8h5umyv.mongodb.net/?appName=NoteCluster'
